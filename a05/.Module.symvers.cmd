db1492546b7c1d4735985e1cee88816bf39e9deb
root <jeanbapt.perrier@gmail.com>
2024-04-11 16:28:35 +0200

    Remove files

--- a05/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/root/ft_penguin/a05/Module.symvers :=  scripts/mod/modpost -M        -o /root/ft_penguin/a05/Module.symvers -n -T /root/ft_penguin/a05/modules.order -i Module.symvers -e 
--- a05/.main.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/root/ft_penguin/a05/main.mod := printf '%s\n'   main.o | awk '!x[$$0]++ { print("/root/ft_penguin/a05/"$$0) }' > /root/ft_penguin/a05/main.mod
